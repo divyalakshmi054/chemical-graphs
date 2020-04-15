dc6113e1790f62c74e2f311cff56f1d003b22a4f
Anish Badri R S <anishbadhri@gmail.com>
2020-04-15 23:54:48 +0530

    Completed variable name change on ErrorMargin

--- Resources/ErrorMargin.py
+++ Resources/ErrorMargin.py
@@ -2,13 +2,12 @@ import abc
 import Resources.GeometricEntities as Ge
 
 
-# TODO(anishbadhri): Rename self x, y to xmargin, ymargin
 class ErrorInterface(abc.ABC):
 
     def __init__(self, x: float, y: float) -> None:
         assert x >= 0 and y >= 0
-        self.x = x
-        self.y = y
+        self.x_margin = x
+        self.y_margin = y
 
     @abc.abstractmethod
     def verify(self, p1: Ge.Point, p2: Ge.Point) -> bool:
@@ -20,7 +19,7 @@ class ErrorRectangle(ErrorInterface):
         super().__init__(x, y)
 
     def verify(self, p1: Ge.Point, p2: Ge.Point) -> bool:
-        if abs(p2.x - p1.x) <= self.x and abs(p2.y - p1.y) <= self.y:
+        if abs(p2.x - p1.x) <= self.x_margin and abs(p2.y - p1.y) <= self.y_margin:
             return True
         return False
 
@@ -31,7 +30,7 @@ class ErrorEllipse(ErrorInterface):
         super().__init__(x, y)
 
     def verify(self, p1: Ge.Point, p2: Ge.Point) -> bool:
-        result = ((p2.x - p1.x) ** 2) / (self.x ** 2) + ((p2.y - p1.y) ** 2) / (self.y ** 2)
+        result = ((p2.x - p1.x) ** 2) / (self.x_margin ** 2) + ((p2.y - p1.y) ** 2) / (self.y_margin ** 2)
         if result <= 1:
             return True
         return False
